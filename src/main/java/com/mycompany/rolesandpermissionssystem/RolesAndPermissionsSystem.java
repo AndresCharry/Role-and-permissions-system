0ebb568a2303ce2b1c4261ed7054ff0ed615df7e
Andres Charry <ingeniero.andres.charry@gmail.com>
2024-03-16 19:28:13 -0500

    V0.1

---
+++ src/main/java/com/mycompany/rolesandpermissionssystem/RolesAndPermissionsSystem.java
@@ -0,0 +1,11 @@
+package com.mycompany.rolesandpermissionssystem;
+
+import com.mycompany.rolesandpermissionssystem.gui.MainScreen;
+
+public class RolesAndPermissionsSystem {
+	public static void main(String[] args) {
+		MainScreen main = new MainScreen();
+		main.setVisible(true);
+		main.setLocationRelativeTo(null);
+	}
+}
